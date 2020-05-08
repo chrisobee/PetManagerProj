9f7d26c98174dcfba832ba7caf397cd6e73003b3
Chris O'Brien <chrisobrien2000@outlook.com>
2020-05-08 16:32:33 -0500

    Added DbSets for all tables and Seeded AnimalType Table

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -16,10 +16,24 @@ namespace PetManager.Data
         }
 
         public DbSet<PetOwner> PetOwners { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetHumanJxn> PetOwnerships { get; set; }
+        public DbSet<AnimalType> AnimalTypes { get; set; }
+        public DbSet<ToDoTask> Tasks { get; set; }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<AnimalType>()
+                .HasData(
+                    new AnimalType { Name = "Dog" },
+                    new AnimalType { Name = "Cat" },
+                    new AnimalType { Name = "Small Mammal" },
+                    new AnimalType { Name = "Fish" },
+                    new AnimalType { Name = "Bird" },
+                    new AnimalType { Name = "Equine" }
+                    );
 
             builder.Entity<IdentityRole>()
                 .HasData(
