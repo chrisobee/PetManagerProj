b25cfcd8dd8a5cd07b73fe79ee14aef8d3a47db1
Christopher Reding <creding21@gmail.com>
2020-05-12 13:32:52 -0500

    refresh

---
+++ Services/GoogleAPI.cs
@@ -0,0 +1,16 @@
+﻿using PetManager.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetManager.Services
+{
+    public class GoogleAPI : IGoogleAPIs
+    {
+        public string GetOwnersCoordinates()
+        {
+            return "string";
+        }
+    }
+}
